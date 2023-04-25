4e9b0b9b43ebcae7738f6a9235c4da072d9e2dd9
hudhuud <118181770+hudhuud@users.noreply.github.com>
2023-04-25 13:27:28 +0300

    Add files via upload

---
+++ двумерные массивы.cs
@@ -0,0 +1,85 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк: ");
+        int rows = int.Parse(Console.ReadLine());
+        Console.Write("Введите количество столбцов: ");
+        int columns = int.Parse(Console.ReadLine());
+
+        int[,] arr = new int[rows, columns];
+        Random rnd = new Random();
+
+        Console.WriteLine("Исходный массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                arr[i, j] = rnd.Next(1, 101); // заполнение массива случайными числами от 1 до 100
+                Console.Write(arr[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // сортировка массива по возрастанию
+        int[] tempArr = new int[rows * columns];
+        int k = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                tempArr[k] = arr[i, j];
+                k++;
+            }
+        }
+        Array.Sort(tempArr, (i, j) => j.CompareTo(i));
+        k = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                arr[i, j] = tempArr[k];
+                k++;
+            }
+        }
+
+        Console.WriteLine("Отсортированный массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(arr[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // замена элементов с чётными индексами элементами с нечётными индексами
+        float[,] shiftedArr = new float[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                 if (j%2==0) // если индекс четный, то меняем элементы
+                {
+                    shiftedArr[i, j] = 1f/arr[i, j];
+                }
+                else // иначе оставляем элемент без изменения
+                {
+                    shiftedArr[i, j] = arr[i, j];
+                }
+            }
+        }
+
+        Console.WriteLine("Массив со заменой:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(Math.Round(shiftedArr[i, j], 2)+ " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ одномерные массивы.cs
@@ -0,0 +1,54 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите размерность массива: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        // заполнение массива случайными числами от 1 до 100
+        Random rand = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = rand.Next(1, 101);
+        }
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(arr);
+
+        // поиск наименьшего и наибольшего элементов
+        int minIndex = 0;
+        int maxIndex = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] < arr[minIndex])
+            {
+                minIndex = i;
+            }
+            if (arr[i] > arr[maxIndex])
+            {
+                maxIndex = i;
+            }
+        }
+
+        // вычисление разности позиций наименьшего и наибольшего элементов
+        int diff = Math.Abs(minIndex - maxIndex);
+
+        Console.WriteLine($"Наименьший элемент: {arr[minIndex]}, его позиция: {minIndex+1}");
+        Console.WriteLine($"Наибольший элемент: {arr[maxIndex]}, его позиция: {maxIndex+1}");
+        Console.WriteLine($"Разность позиций: {diff}");
+    }
+
+    // метод для вывода элементов массива в консоль
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
